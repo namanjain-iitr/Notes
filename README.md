0f4dd7a481e4324db889b8bd7aaee7ac38b079e3
Naman Jain <jnamanjain2001@gmail.com>
2023-06-04 09:48:16 +0530

    uibuild

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Notes
 repository to contain code for notes app from fullstackopen course
+
+render URL:- https://notes-8o45.onrender.com/
---
+++ mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+require("dotenv-safe").config();
+
+const url = process.env.ATLAS_MONGODB_URL;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "Hey there what's up?",
+  important: true,
+});
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
+
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
